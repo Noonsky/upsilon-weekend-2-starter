84477057664ea704a54febb45b615f92a44aa2ba
Erik Noonan <eriknoonan@Eriks-MacBook-Pro.local>
2017-01-14 16:26:18 -0600

    Index and person-info population working
    working to add buttons, keeps breaking in different ways each time I attempt to add them

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,19 +1,8 @@
 //client side code
 // buttons function to be built here outside of and before ajax function
-// function start(){
-//   appendDom(upsilon.json[0]);
-//
-// }
-//
-// $(function(){
-//   $('#next').on('click', appendDom);
-// });
-// //
-// $(function(){
-// appendDom(person[0]);
-// });
-//
 
+var currentIndex=0;
+var people=[];
 
     $(document).ready(function(){
         $.ajax({
@@ -23,6 +12,9 @@
             console.log(response);
             appendDom(response.person[0]);
             buildIndex(response.person.length);
+            var people=response.person;
+            createButtons();
+            // $('#next').on('click', appendDom(people[currentIndex++]));
           }
         });
     });
@@ -42,12 +34,26 @@ function buildIndex(length){
   for (var i=0; i<length;i++){
     $('#indexContainer').append('<div class="indexBox" id=' +i+ '></div>')
   }
+  document.getElementById(currentIndex).style.backgroundColor="red";
 };
+
+
+function createButtons(a){
+a.append("<div class='buttons' id='next'>Next</div>"),
+a.append("<div class='buttons' id='prev'>Previous</div>")
+};
+
+// $(function(){
+//
+// });
+
+
+// currentIndex=currentIndex++;
 
     // 2 buttons (next and previous), a 17 index indicator, and timer
     //doc loads with array object 0 (17 objects in total, 0-16) displayed and index 0 (first index) lit
 
-    //index items correspond to array object numbers, with lit array triggering display of corresponding object data
+    //index items correspond to array object numbers, with lit index triggering display of corresponding object data
 
     //When 'next' is pressed div id="ajax-data" .appends DOM with info of next array object in order unless
     //it has hit the last object (object 16) in which case it will reset to 0
